63824978122541f6875a09b7191d0e4e9c936ab2
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2018-05-31 00:11:47 -0400

    headers working in output

--- convert.py
+++ convert.py
@@ -24,14 +24,18 @@ def make_tuples(input_data):
 
 def make_html(input_tuples):
     """ makes html out of the tuples """
-    with open('output.html', 'a') as f2:
+    with open('output.html', 'a', encoding='utf-16') as f2:
         f2.write('<ul>')
+        item_counter = ''
         for item in input_tuples:
+            if item[0] != item_counter:
+                f2.write('<h3>' + item[0] + '</h3>')
             f2.write(item[1])
+            item_counter = item[0]
         f2.write('</ul>')
 
 if __name__ == "__main__":
-    with open('March2018.csv', 'r') as f1:
+    with open('March2018.csv', 'r', encoding='cp437') as f1:
         DATA = csv.reader(f1)
         tuples = make_tuples(DATA)
     make_html(tuples)
