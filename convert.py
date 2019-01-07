3d1daa73f6989f2594af1e256eafd765d0b46059
MarkEEaton <MarkEEaton@users.noreply.github.com>
2019-01-07 14:48:33 -0500

    edit for clarity

--- convert.py
+++ convert.py
@@ -12,7 +12,7 @@ def make_tuples(input_data):
     for line in input_data[1:]:
 
         # make the HTML for the title. Use the titlecase library for better capitalization
-        bib_info1 = '<li><strong>' + titlecase(line[2]) + '</strong>'
+        bib_info1 = '<strong>' + titlecase(line[2]) + '</strong>'
 
         # make the HTML for the author. Leave out the word 'author'.
         if line[1]:
@@ -24,27 +24,28 @@ def make_tuples(input_data):
                 author = line[1]
             bib_info2 = '<li><em>Author: </em>' + author + '</li>'
         else:
-            # if there is no author, return an empty string
+            # if there is no author, assign an empty string
             bib_info2 = ''
 
         # make the HTML for the call number
         if line[3]:
             bib_info3 = '<li><em>Call number: </em>' + line[3] + '</li>'
         else:
-            # if there is no call number, return an empty string
+            # if there is no call number, assign an empty string
             bib_info3 = ''
 
-        # make the HTML for the catalog link
+        # make the url for the catalog link, then make HTML for the url
         url = 'http://onesearch.cuny.edu/primo-explore/search?query=any,contains,' + \
               line[2] + '&tab=default_tab&search_scope=everything&vid=kb&lang=en_US&offset=0'
         bib_info4 = '<li><a href=\"' + url + '\">Search the catalog</a></li>'
 
         # assemble all the HTML for the item
-        bib_info = bib_info1 + '<ul>' + bib_info2 + bib_info3 + bib_info4 + '</ul></li>'
+        bib_info = '<li>' + bib_info1 + '<ul>' + bib_info2 + bib_info3 + bib_info4 + '</ul></li>'
 
         # make a tuple for the item. The first item is LC Class, the second is the HTML.
-        line_tup = (line[0], bib_info)
-        data_list.append(line_tup)
+        lc_class = line[0]
+        item_tuple = (lc_class, bib_info)
+        data_list.append(item_tuple)
 
     # return the list of tuples
     return data_list
@@ -52,7 +53,7 @@ def make_tuples(input_data):
 def make_html(input_tuples, outfile):
     """ makes html out of the tuples """
 
-    # make a file for the HTML
+    # open a file for the HTML
     with open('data/' + outfile, 'a', encoding='utf-16') as file_2:
 
         # start a big unordered list
@@ -76,7 +77,7 @@ def make_html(input_tuples, outfile):
 def main():
     """ run it! """
 
-    # handle the command line input
+    # handle the command line input using argparse
     parser = argparse.ArgumentParser(description='Parses a csv of new books')
     parser.add_argument('infile', metavar='[infile]', type=str,
                         help='a csv file')
